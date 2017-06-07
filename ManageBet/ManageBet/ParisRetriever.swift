2d22338f02dccea8a8b5cc59c7cfbfe2d5a402e3
etudiant <etudiant@iMac-de-etudiant-57.local>
2017-06-07 15:14:18 +0200

    Commit project

---
+++ ManageBet/ManageBet/ParisRetriever.swift
@@ -0,0 +1,13 @@
+//
+//  ParisRetriever.swift
+//  ManageBet
+//
+//  Created by etudiant on 07/06/2017.
+//  Copyright © 2017 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ParisRetriever: NSObject {
+
+}
---
+++ ManageBet/ManageBet/ParisViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ParisViewController.swift
+//  ManageBet
+//
+//  Created by etudiant on 07/06/2017.
+//  Copyright © 2017 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ParisViewController: UIViewController, UIPickerViewDelegate {
+
+    @IBOutlet weak var pickSport: UIPickerView!
+    
+    var listSport: Dictionary<String, Int> = ["Football" : 100, "Tennis":600]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        <#code#>
+    }
+
+}
