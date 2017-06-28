8f004375cee3a2bdfc1f2151338a6a5492a1a854
teddy.reino9@gmail.com <teddy.reino9@gmail.com>
2017-06-28 12:37:10 +0200

    Revert "Merge branch 'dev'"
    This reverts commit 67405c95ace60ac002405457a91eaff52c750968.

--- ManageBet/ParisViewController.swift
+++ ManageBet/ParisViewController.swift
@@ -8,40 +8,16 @@
 
 import UIKit
 
-class ParisViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
+class ParisViewController: UIViewController, UIPickerViewDelegate {
 
     @IBOutlet weak var pickSport: UIPickerView!
-    @IBOutlet weak var matchTableView: UITableView!
     
-    var listSport: Array<String> = ["Football", "Tennis"]
-    var listSportForAPI: Dictionary<String, Int> = ["Football" : 100, "Tennis":600]
-    
-    let parisRetriever: ParisRetriever = ParisRetriever()
+    var listSport: Dictionary<String, Int> = ["Football" : 100, "Tennis":600]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        pickSport.dataSource = self
-        pickSport.delegate = self
+
         // Do any additional setup after loading the view.
-    }
-    
-    func getParis(sport: Int){
-        parisRetriever.getParis(block: { (parisList, error) in
-            if parisList != nil {
-                parisList?.forEach({(item: AnyObject) in
-                    let id: String = item["sportId"]! as! String
-                    NSLog("%s", id)
-                })
-                DispatchQueue.main.async(execute: {
-                    self.matchTableView.reloadData()
-                })
-            }
-        }, sport: sport)
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,15 +25,8 @@ class ParisViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         // Dispose of any resources that can be recreated.
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return listSport.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return listSport[row]
-    }
-    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        getParis(sport: listSportForAPI[listSport[row]]!)
+        
     }
+
 }
